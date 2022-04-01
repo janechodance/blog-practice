b0ddef312ffa22b1c75fbe59e72a114868a1d2b3
Jane <janechodance@gmail.com>
2022-04-01 18:56:32 -0400

    action creaters

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) =>{
+   await dispatch(fetchPosts());
+   const userIds = _.uniq(_.map(getState().posts, 'userId'))
+   userIds.forEach(id=> dispatch(fetchUser(id)))
+
+};
 export const fetchPosts = ()=>{
     return async (dispatch)=> {
         const response = await jsonPlaceholder.get('/posts');
@@ -15,3 +23,4 @@ export const fetchUser= (id)=> async dispatch=> {
 
     dispatch({type: 'FETCH_USER', payload: response.data})
 }
+
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {connect} from 'react-redux';
-import {fetchPosts} from '../actions'
+import {fetchPosts, fetchPostsAndUsers} from '../actions'
 import UserHeader from "./UserHeader";
 
 class PostList extends React.Component{
     componentDidMount(){
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
     renderList(){
         return this.props.posts.map(post =>{
@@ -32,4 +32,4 @@ const mapStateToProps = (state)=>{
     return {posts: state.posts}
 }
 
-export default connect(mapStateToProps, {fetchPosts})(PostList)
+export default connect(mapStateToProps, {fetchPosts, fetchPostsAndUsers})(PostList)
